c93090fdc2a4469cb4009b118fc09df2322a0b26
Vinamilk's <toinv2610@gmail.com>
2022-11-10 18:56:10 +0700

    update res

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ $("#submit").click(() => {
   const value = $("#textContent").val();
   $("#text").text(value);
   const widthText = $("#text").width();
+  const maxWidth = $(window).outerWidth() > 425 ? 200 : 150;
+  console.log("maxWidth :>> ", maxWidth);
   if (widthText > 200) {
-    $("i").css("font-size", `${widthText + 40}px`);
+    $("i").css(
+      "font-size",
+      `${widthText > $(window).outerWidth() ? widthText + 40 : maxWidth}px`
+    );
   } else {
-    $("i").css("font-size", `${200}px`);
+    $("i").css("font-size", `${maxWidth}px`);
   }
 });
